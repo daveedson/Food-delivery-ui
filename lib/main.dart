48a55bfeb8b852228563508ef0cf292836497930
daveedson <daveeddrumz@gmail.com>
2020-05-04 12:19:24 +0100

    Added responsive_FrameWork package for easy layout scalling

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:responsive_framework/responsive_framework.dart';
 import 'Screens/HomeScreen.dart';
 
 void main() => runApp(MyApp());
@@ -9,13 +9,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    title: 'Food Delivery',
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
+     builder: (context, widget) => ResponsiveWrapper.builder(
+       widget,
+          maxWidth: 1200,
+          minWidth: 450,
+          defaultScale: true,
+          breakpoints: [
+            ResponsiveBreakpoint(breakpoint: 450, name: MOBILE),
+            ResponsiveBreakpoint(breakpoint: 800, name: TABLET, autoScale: true),
+            ResponsiveBreakpoint(breakpoint: 1000, name: TABLET, autoScale: true),
+            ResponsiveBreakpoint(breakpoint: 1200, name: DESKTOP),
+            ResponsiveBreakpoint(breakpoint: 2460, name: "4K", autoScale: true),
+          ],
+          background: Container(color: Color(0xFFF5F5F5))),
+           title: 'Food Delivery',
+             debugShowCheckedModeBanner: false,
+              theme: ThemeData(
         scaffoldBackgroundColor: Colors.grey[50],
         primaryColor: Colors.lightBlue,
     ) ,
-    home: HomeScreen(),
+      home: HomeScreen(),
+
+      
     );
   }
 }
